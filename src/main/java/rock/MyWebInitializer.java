4a0487ddb0809e62af7ded24d85ccf270c386a3c
jjorae <raehyeok.cho@gmail.com>
2017-01-17 15:48:53 +0900

    Tomcat 설정

---
+++ src/main/java/rock/MyWebInitializer.java
@@ -0,0 +1,11 @@
+package rock;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class MyWebInitializer extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FirstDemoApplication.class);
+	}
+}
